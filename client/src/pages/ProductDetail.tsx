cc9b2ba45a52c0334fc81a9f7006535f45827057
indraprhmbd <indraprhmbd@gmail.com>
2026-09-12 17:10:04 +0700

    fix(client): smaller QRIS logo right of checkout label

--- client/src/pages/ProductDetail.tsx
+++ client/src/pages/ProductDetail.tsx
@@ -398,10 +398,10 @@ export default function ProductDetail() {
               <button
                 onClick={confirmBuy}
                 disabled={purchasing}
-                className="flex-1 btn btn-primary border-2 border-black font-black text-xs uppercase py-2 btn-comic-interactive disabled:opacity-50 flex flex-col items-center justify-center gap-1 leading-none"
+                className="flex-1 btn btn-primary border-2 border-black font-black text-xs uppercase py-2.5 btn-comic-interactive disabled:opacity-50 flex items-center justify-center gap-2 leading-none"
               >
                 <span>{purchasing ? t.products.processing : t.products.confirmGo}</span>
-                {!purchasing && <img src="/QRIS_logo.svg" alt="QRIS" className="h-3.5 w-auto" />}
+                {!purchasing && <img src="/QRIS_logo.svg" alt="QRIS" className="h-3 w-auto" />}
               </button>
             </div>
           </div>
